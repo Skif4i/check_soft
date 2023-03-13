b9ba8f60f31f5328fba9afad51ada4efc0c673b2
skif4i <skif4i@internet.ru>
2023-03-13 22:47:43 +0500

    start

---
+++ page_open/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="/image/cube_icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="/css/link.css">
+    <meta charset="UTF-8">
+    <title>SOFT-PORTALER</title>
+</head>
+
+<header>
+    <h1>Только честный софт</h1>
+<h8><?php echo date('d.m.Y')?></h8>
+</header>
+<body>
+
+<main>
+    <p>Мир софта постоянно развивается и каждый месяц выходят новые программы,
+        которые удобнее и эффективнее тех, к которым мы привыкли. Поэтому стоит иногда пробовать новинки,
+        ведь среди них можно найти полезные утилиты, которые позволят вам решать проблемы с ПК и работать на нем с большим комфортом.
+        Коллекция утилит на моем жестком диске постоянно обновляется и сегодня я познакомлю вас с десятком интересных экземпляров,
+        которые мне понравились в 2023 году. </p>
+
+    <hr>
+    <a href="microsoftbasik.html"  rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Microsoft Набор ПО</span></button></a>
+
+    <a href="#" download="" rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Driver Booster</span></button></a>
+
+    <a href="hds.html"  rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Hard Disk Sentinal</span></button></a>
+
+    <a href="#" download="" rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>WebStorm+Activate</span></button></a>
+
+    <a href="dx.html"  rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Winrar</span></button></a>
+
+    <a href="Activate.html"  rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Активатор Win-10</span></button></a>
+
+    <a href="#" download="" rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Windows 10</span></button></a>
+
+    <a href="#" download="" rel="nofollow" target="_blank"><button class="custom-btn btn-12">
+        <span>Скачать</span><span>Mova Vi Editor</span></button></a>
+
+    <hr>
+
+
+
+
+</main>
+</body>
+</html>
